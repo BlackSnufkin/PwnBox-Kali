12ad27b44d459b53d6953429bb3b22cdcb89ade0
BlackSnufkin <61916899+BlackSnufkin@users.noreply.github.com>
2021-10-27 00:38:41 +0300

    Update domain_user_parser.sh

--- General/domain_user_parser.sh
+++ General/domain_user_parser.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-#parse domain user output from ldapdomaindump to generate a users list for passwordspray and etc.
-cat domain_users.json|grep -i samaccountname -A 1 >> ../domain_users.lst;sed -i '1 i\t' ../domain_users.lst;sed -i -n '0~3p' ../domain_users.lst;sed -i 's/ *//g' ../domain_users.lst;sed -i 's/"//g' ../domain_users.lst
+## Parse Domain user output from ldapdomaindump to generate a users list for passwordspray and etc.
+cat domain_users.json|grep -i samaccountname -A 1 >> ../users.lst;sed -i '1 i\t' ../users.lst;sed -i -n '0~3p' ../users.lst;sed -i 's/ *//g' ../users.lst ;sed -i 's/"//g' ../users.lst;cat ../users.lst | sort > ../Domain_Users.lst;rm ../users.lst
