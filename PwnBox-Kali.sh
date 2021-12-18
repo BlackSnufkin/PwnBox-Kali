5e8ec4d83036959bd2b1e3832613b7408af78d29
blacksnufkin <blacksnufkin42@gmail.com>
2021-12-18 13:20:57 +0200

    small fixing

--- PwnBox-Kali.sh
+++ PwnBox-Kali.sh
@@ -1422,7 +1422,6 @@ if [ $? == 0 ]; then
     systemctl enable postgresql
 
     SpinUp_Workspace
-    Utils-Tools
 
     Recon-Tools
     Web-Tools
@@ -1438,6 +1437,8 @@ if [ $? == 0 ]; then
     ActiveDirectory_CredentialDumping-Tools
     ActiveDirectory_Offensive-Tools
     Exploits
+
+    Utils-Tools
     Twiking
 
     Update && apt reinstall python3-debian -y && check_reboot
